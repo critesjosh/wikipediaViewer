c569dfc8fa5266355e06d49fe1ad7e875c944bd5
Josh <critesjosh@gmail.com>
2016-07-09 22:31:04 -0600

    Saves JSON response
    Added code to save and store the JSON response from Wikipedia.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var searchTerm = 'bananas';
 
+var titles = [];
+var descrptions = [];
+var links = [];
+
+var sendRequest = function(searchTerm){
+
     $.ajax( "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=wikiCallback", {
       dataType: 'jsonp'
       }).done(function(data){
-      console.log(data);
+      data[1].forEach(function(element){
+        titles.push(element);
+      });
+      data[2].forEach(function(element){
+        descrptions.push(element);
+      });
+      data[3].forEach(function(element){
+        links.push(element);
+      });
     });
+}
